698df8e7226ca57247129aa030827ff0460a3d00
Zeger Use <90973852+ZegerUser@users.noreply.github.com>
2025-12-05 11:00:45 +0100

    docker setup

---
+++ timescaledb_init/init-db.sql
@@ -0,0 +1,27 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
+CREATE TABLE machine_sensors (
+    time        TIMESTAMPTZ NOT NULL,
+    device_id   INT         NOT NULL,
+    device_type TEXT        NOT NULL,
+    sensor_type TEXT        NOT NULL,
+    sensor_val  DOUBLE PRECISION,
+    location    TEXT        NULL,
+    PRIMARY KEY (time, device_id)
+);
+SELECT create_hypertable('machine_sensors', 'time');
+SELECT add_retention_policy('machine_sensors', INTERVAL '90 days');
+
+CREATE TABLE sensor_aggregates (
+    time        TIMESTAMPTZ NOT NULL,
+    device_id   INT         NOT NULL,
+    device_type TEXT        NOT NULL,
+    sensor_type TEXT        NOT NULL,
+    average_val DOUBLE PRECISION NULL,
+    min_val     DOUBLE PRECISION NULL,
+    max_val     DOUBLE PRECISION NULL,
+    count       INTEGER     NOT NULL,
+    PRIMARY KEY (time, device_id)
+);
+SELECT create_hypertable('sensor_aggregates', 'time');
+SELECT add_retention_policy('sensor_aggregates', INTERVAL '90 days');
